3078be5819f4c3a1898ebb5e21e28a1c3aa44259
bukitoo <bukitoo@gmail.com>
2017-02-27 20:45:06 -0300

    - Correcciones a version clase 1

--- src/main.c
+++ src/main.c
@@ -1,36 +1,3 @@
-/* Copyright 2016, Pablo Ridolfi
- * All rights reserved.
- *
- * This file is part of Workspace.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are met:
- *
- * 1. Redistributions of source code must retain the above copyright notice,
- *    this list of conditions and the following disclaimer.
- *
- * 2. Redistributions in binary form must reproduce the above copyright notice,
- *    this list of conditions and the following disclaimer in the documentation
- *    and/or other materials provided with the distribution.
- *
- * 3. Neither the name of the copyright holder nor the names of its
- *    contributors may be used to endorse or promote products derived from this
- *    software without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
- * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
- * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
- * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
- * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
- * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
- * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
- * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
- * POSSIBILITY OF SUCH DAMAGE.
- *
- */
-
 #include "main.h"
 #include "board.h"
 #include <string.h>
@@ -51,7 +18,8 @@ uint32_t state;
 void task1( void )
 {
     int i;
-    while ( 1 )
+
+    while( 1 )
     {
         Board_LED_Toggle( 0 );
         for ( i=0; i<0x3FFFFF; i++ );
@@ -61,28 +29,24 @@ void task1( void )
 void task2( void )
 {
     int j;
-    while ( 1 )
+
+    while( 1 )
     {
         Board_LED_Toggle( 2 );
         for ( j=0; j<0xFFFFF; j++ );
     }
 }
 
-void return_hook( void )
-{
 
-}
-
-void taskSetup( void ( *entry_point )( void ), uint32_t * stack,
-                uint32_t stack_size, uint32_t ** sp )
+void taskSetup( void ( *entry_point )( void ), uint32_t * stack, uint32_t stack_size, uint32_t ** sp )
 {
     bzero( stack, stack_size );
 
-    stack[stack_size/4-3] = ( uint32_t )return_hook; /* LR */
-    stack[stack_size/4-2] = ( uint32_t )entry_point; /* PC */
-    stack[stack_size/4-1] = 1<<24; /* xPSR.T = 1 */
+    stack[stack_size/4-3] = ( uint32_t ) return_hook; 	/* LR */
+    stack[stack_size/4-2] = ( uint32_t ) entry_point; 	/* PC */
+    stack[stack_size/4-1] = 1<<24; 						/* xPSR.T = 1 */
 
-    *sp = stack+stack_size/4-8; /* sp inicial */
+    *sp = stack+stack_size/4 - 16; 						/* sp inicial */
 }
 
 int main( void )
